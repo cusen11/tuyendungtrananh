ab321289712c94d79a2fde30218429784a02034b
cusen11 <you@example.com>
2022-01-11 16:19:30 +0700

    done

--- src/Component/Ourteam/Ourteam.jsx
+++ src/Component/Ourteam/Ourteam.jsx
@@ -22,10 +22,20 @@ function Ourteam() {
             {
               breakpoint: 1024,
               settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                infinite: true,
+                dots: true,
+                arrows:false,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
-                infinite: true,
-                dots: true
+                initialSlide: 3,
+                arrows:false,
               }
             },
             {
@@ -33,14 +43,16 @@ function Ourteam() {
               settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2,
-                initialSlide: 2
+                initialSlide: 2,
+                arrows:false,
               }
             },
             {
               breakpoint: 480,
               settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToScroll: 2,
+                arrows:false,
               }
             }
           ]
